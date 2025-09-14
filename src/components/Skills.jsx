1684993aec9a483ba901db7e98fcc939181770d5
Ketan Bidave <ketanpc77@gmail.com>
2025-09-14 16:12:37 +0530

    image changes

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,14 +1,3 @@
-import c from "../assets/images/C.png";
-import cpp from "../assets/images/C++.png";
-import css from "../assets/images/css.png";
-import html from "../assets/images/html.png";
-import java from "../assets/images/java.png";
-import js from "../assets/images/js.png";
-import py from "../assets/images/python.png";
-import react from "../assets/images/react.png";
-import spring from "../assets/images/Spring.png";
-import sql from "../assets/images/sql.png";
-import Github from "../assets/images/github.png"
 import { IoLogoReact } from "react-icons/io5";
 import { TbBrandCpp } from "react-icons/tb";
 import { FaPython } from "react-icons/fa";
@@ -22,20 +11,6 @@ import { RiTailwindCssFill } from "react-icons/ri";
 import { FaGithub } from "react-icons/fa";
 import { motion } from "framer-motion"
 
-
-const skills = [
-  { src: c, alt: "C" },
-  { src: cpp, alt: "C++" },
-  { src: py, alt: "Python" },
-  { src: java, alt: "Java" },
-  { src: spring, alt: "Spring"},
-  { src: sql, alt: "SQL"},
-  { src: react, alt: "React"},
-  { src: html, alt: "HTML" },
-  { src: css, alt: "CSS" },
-  { src: js, alt: "JS" },
-  { src: Github, alt: "GitHub" }
-];
 
 const Skills = () => (
   <div className="about card">
