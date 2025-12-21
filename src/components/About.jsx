ec2888cbcfd023c14cf2c15a1864f059f19185ad
Ketan Bidave <ketanpc77@gmail.com>
2025-12-21 18:37:34 +0530

    mindcalc url update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,10 +6,10 @@ const About = () => (
     <motion.div initial={{opacity: 0, y: 50}} whileInView={{opacity: 1, y: 0}} transition={{duration: 0.8}} viewport={{once: true}}>
     <p >
       I am a software developer with a strong foundation in Java and hands-on
-      experience in <font color="cyan">Spring Boot, Servlets/JSP, JDBC, SQL, HTML, CSS, and React. </font>
+      experience in Spring Boot, Servlets/JSP, JDBC, SQL, HTML, CSS, and React.
       My programming journey began with C and C++, but I quickly discovered my
       passion for Java and have since focused on mastering its ecosystem—covering
-      <font color="cyan"> MVC, JPA, AOP, OAuth, JWT, and REST API development for scalable applications. </font>
+      MVC, JPA, AOP, OAuth, JWT, and REST API development for scalable applications.
       Alongside backend development, I am steadily expanding my skills in deployment
       practices, Docker, and cloud platforms to build reliable and production-ready solutions.
       With a solid understanding of core concepts like object-oriented programming, 
